a270b8ae6f9dba6660eb8a13be86ec82180d2327
Cheryl Wichman <camandche@gmail.com>
2020-08-02 22:22:45 +1200

    Tidying up HTML Code

--- JavaScript/Whack-a-Mole/whack-a-mole.js
+++ JavaScript/Whack-a-Mole/whack-a-mole.js
@@ -1,5 +1,87 @@
-  // -    -   -   -   -  //
+ // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+    //Store or get references to the cells in HTML table
+var cells = document.querySelectorAll('td');
+// var cells = document.getElementsByTagName('td')
+
+var randomIndex = Math.floor(Math.random() * cells.length)
+var randomCell = cells[randomIndex]
+
+//Show mole in chosen cell
+//First, create image object
+const mole = document.createElement('img');
+// mole.setAttribute("id", "mole")
+// mole.className = "mole";
+
+mole.src = './mole.png';
+mole.id = 'mole';
+
+//Append mole to 'randomCell'
+randomCell.appendChild(mole);
+
+mole.onclick = whackedMole;
+
+//function showMole(){
+
+   // var img = document.getElementById("img");
+    //var randomIndex = Math.floor(math.random() *cells.length);
+    //var randomCell = tableArray[randomIndex];
+
+    //Append img to cell
+    // //table[randomCell()].appendChild(mole);
+    // mole.addEventListener("Click", randomMole);
+    // cells[Math.floor(Math.random() * cells.length)];
+    // setInterval(() => {
+    // mole();    
+    // },
+    // 1000);
+    // randomMole();
+//}
+
+//Create 'whackedMole' Function
+function whackedMole() {
+    var randomIndex = Math.floor(Math.random() * cells.length)
+    randomCell = cells[randomIndex]
+    randomCell.appendChild(mole);
+
+//Listen out for when 'whackedMole' is clicked, then play audio    
+var audio = new Audio('./whack-audio.wav');
+audio.play();
+}
+
+    //Add Click Counter
+    var click = 0;
+    function onclick() {
+    click +=1;
+    document.getElementById("click").innerHTML = click;
+}
+
+console.log("Whack-a-Mole!");
+
+// function showMole (){   // document.getElementsByID("mole").appendChild(mole);
+//    }
+// document.getElementsByID("mole").appendChild(mole);
+
+//}
+
+// const randomCell = () => cells[Math.floor(Math.random()*cells.length)];
+// const showMole = () => randomCell().appendChild(mole);
+
+// mole.addEventListener("click", showMole);
+// setInterval(() => {
+//     showMole();
+
+// var tds = document.getElementsByTagName("td");
+
+// setInterval(function(){
+// 	var randomNumber = Math.floor(Math.random() * tds.length);
+// 	tds[randomNumber].appendChild(img);
+// // }, 2000);
+// function clearAll(){
+//     var elements = document.getElementsByTagName('td');
+//     for (var i=0;i<elements.length;i++){
+//       elements[i].innerHTML="<p></p>";
+//     }
+//   }
--- JavaScript/dress-the-clown/dress-the-clown.js
+++ JavaScript/dress-the-clown/dress-the-clown.js
@@ -3,3 +3,104 @@
 // -    -   -   -   -  //
 
 console.log("Dress The Clown!")
+
+//Write function to dress clowns head
+
+var head = 0
+var body = 0
+var shoes = 0
+
+var head = document.getElementById("head");
+var body = document.getElementById("body");
+var shoes = document.getElementById("shoes");
+
+var headIndex = 0
+var bodyIndex = 0
+var shoesIndex = 0
+
+var mainIndex = 0
+var moveAroundBody = 0
+
+//Listen out for arrow keystrokes; assign to a function
+document.addEventListener("keydown", function(event) {
+  //Right Arrow
+  if (event.keyCode == 39) {
+    event.preventDefault();
+    console.log ("Right arrow pressed");
+    changeRight(1);
+  }
+  //Left Arrow
+  else if (event.keyCode == 37) {
+    event.preventDefault();
+    console.log ("Left arrow pressed");
+    changeLeft(-1);
+  }
+  //Up Arrow
+  else if (event.keyCode == 38) {
+    console.log ("Up arrow pressed");
+    event.preventDefault();
+    moveUpBody(-1);
+  }
+  //Down Arrow
+  else if (event.keyCode == 40) {
+    console.log ("Down arrow pressed");
+    event.preventDefault();
+    moveDownBody(1);
+  }
+  });
+
+//Function using right arrow to change up through items of clothes 
+  function changeRight() {
+    if (mainIndex == 0) {
+      if (headIndex < 5) {
+        headIndex ++;
+        head.src = "./images/head" + headIndex + ".png"
+      }
+    }
+    else if (mainIndex == 1) {
+      if (bodyIndex < 5) {
+        bodyIndex ++;
+        body.src = "./images/body" + bodyIndex + ".png"
+      }
+    }
+    else if (mainIndex == 2) {
+    if (shoesIndex < 5) {
+        shoesIndex ++;
+        shoes.src = "./images/shoes" + shoesIndex + ".png"
+      }
+    }
+  }
+
+//Function using left arrow to change down through items of clothes
+function changeLeft() {
+  if (mainIndex == 0) {
+    if (headIndex > 0) {
+      headIndex --;
+      head.src = "./images/head" + headIndex + ".png"
+    }
+  }
+    
+  else if (mainIndex == 1) {
+    if (bodyIndex > 0) {
+      bodyIndex --;
+      body.src = "./images/body" + bodyIndex + ".png"
+    }
+  }
+
+  else if (mainIndex == 2) {
+    if (shoesIndex > 0) {
+      shoesIndex --;
+      shoes.src = "./images/shoes" + shoesIndex + ".png"
+    }
+  }
+}
+  
+function moveUpBody() {
+  if (mainIndex > 0)
+  mainIndex--
+}
+
+function moveDownBody() {
+  if (mainIndex < 2)
+  mainIndex++
+}
